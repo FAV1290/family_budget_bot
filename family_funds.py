cf6fd0e59f8dd8000a577b1e00d18ed79359a1cf
Alexey <fav1290@gmail.com>
2023-06-19 00:45:53 +0300

    Various fixes and add-ons for outline

--- family_funds.py
+++ family_funds.py
@@ -1,59 +1,125 @@
 import datetime
+import json
+import uuid
 from constants import COMMANDS
+
+
+def get_default_data():
+    user_dict = {
+	    'expenses' : [],
+	    'categories' : ['разное'],
+	    'is_app_configured' : False,
+        'utc_offset' : 3,
+    }
+    return user_dict
+
+
+def save_user_data(data_dict, user_id):
+    with open(f'{user_id}.json', 'w') as json_handler:
+        json.dump(data_dict, json_handler)
+
+
+def load_user_data(user_id):
+    try:
+        with open(f'{user_id}.json', 'r', encoding='utf-8') as json_handler:
+            data_dict = json.load(json_handler)
+    except OSError:
+        data_dict = get_default_data()
+    return data_dict
 
 
 def get_command_descriptions():
     commands_message = ''
     for command in COMMANDS.values():
-        commands_message += f"{command}\n"
+        commands_message += f'{command}\n'
     return commands_message
 
 
-def check_outlay_string(user_string):
+def check_expense_string(user_string):
     user_string = user_string.split()
     feedback = 'ok'
     if len(user_string) == 1:
-        return f'Вы забыли указать сумму. Примеры правильного ввода:\n"/sum 100 мороженка" или "/sum 500".'
+        return f'Вы забыли указать сумму. Примеры правильного ввода:\n"/add 100 мороженка" или "/add 500"'
     try:
         if int(user_string[1]) <= 0:
-            feedback = f'Сумма расхода должна быть больше нуля.'
+            feedback = f'Вы указали некорректную сумму. Сумма расхода должна быть больше нуля'
     except ValueError:
-        feedback = f'Задана некорректная сумма. Примеры правильного ввода:\n"/sum 100 мороженка" или "/sum 500".'
+        feedback = f'Задана некорректная сумма. Примеры правильного ввода:\n"/add 100 мороженка" или "/add 500"'
     return feedback
 
 
-def make_outlay(user_string):
+def make_expense(user_string):
     sum = user_string.split()[1]
     if len(user_string.split()) == 2:
         description = None
     else:
         description = user_string[(len(sum) + 6):]
-    outlay = {
-        'id' : '',  # Продумать и добавить генерацию id позднее
-        'when' : datetime.datetime.now(),   # В дальнейшем заменить на UTC
+    expense = {
+        'id' : str(uuid.uuid4()),
+        'when' : datetime.datetime.utcnow().strftime('%d.%m.%y, %H:%M'),
         'sum' : sum,
-        'category' : '',    # Когда я освою InlineKeyboardMarkup, здесь появятся категории
+        'category' : 'разное',
         'description' : description,
     }
-    return outlay
+    return expense
 
 
-def add_outlay(outlays_list, new_outlay):
-    if outlays_list is None:
-        outlays_list = []
-    outlays_list.append(new_outlay)
-    feedback = f"Новая расходная операция:\n"
-    feedback += f"Сумма: {new_outlay['sum']} руб."
-    if new_outlay['description'] is not None:
-        feedback += f"\nКомментарий: {new_outlay['description']}"
-    return outlays_list, feedback
+def add_expense(expenses_list, new_expense):
+    if expenses_list is None:
+        expenses_list = []
+    expenses_list.append(new_expense)
+    feedback = f"Новая расходная операция на сумму {new_expense['sum']} рублей.\n"
+    if new_expense['description'] is not None:
+        feedback += f"Комментарий: {new_expense['description']}"
+    feedback += f'\n Пожалуйста, выберите категорию расхода:'
+    return expenses_list, feedback
 
 
-def format_outlays_report(outlays_list):
-    report = ''
-    for outlay in outlays_list:
-        report += f"• ({outlay['when'].strftime('%d.%m.%y, %H:%M')}): -{outlay['sum']} руб."
-        if outlay['description'] is not None:
-            report += f" ({outlay['description']})"
-        report += '\n'
+def add_category(categories_list, user_string):
+    new_category = user_string[8:].lower().strip()
+    if categories_list is None:
+        categories_list = get_default_data()['categories']
+    categories_list.append(new_category)
+    feedback = f'Добавлена категория {new_category.capitalize()}'
+    return categories_list, feedback
+
+
+def add_categories(categories_list, user_string):
+    feedback = 'Добавлены следующие категории: '
+    for category in user_string.split(','):
+        if category[0] == '/':
+            category = category[9:]
+        category = category.lower().strip()
+        categories_list.append(category)
+        feedback += f'{category.capitalize()}, '
+    return categories_list, feedback[:-2]
+
+
+def show_categories(categories_list):
+    if categories_list is None:
+        categories_list = get_default_data()['categories']
+    report = 'Вам доступны следующие категории расходов:'
+    for category in sorted(categories_list):
+        report += f'\n • {category.capitalize()}'
     return report
+
+
+def format_expenses_report(expenses_list, user_id):
+    report = ''
+    utc_offset = datetime.timedelta(hours=load_user_data(user_id)['utc_offset'])
+    for expense in expenses_list:
+        when = datetime.datetime.strptime(expense['when'], '%d.%m.%y, %H:%M')
+        report += f"• ({(when + utc_offset).strftime('%d.%m.%y, %H:%M')}) ({expense['category'].capitalize()}):  -{expense['sum']} руб. "
+        if expense['description'] is not None:
+            report += f"\n{expense['description']}"
+        report += '\n'
+    if report == '':
+        report = 'Расходов не найдено. Везет же!'
+    else:
+        report = 'Cписок ваших расходов:\n' + report
+    return report
+
+
+def find_expense_num_by_id(expenses_list, target_id):
+    expense_num = next((num for num, expense in enumerate(expenses_list) if expense['id'] == target_id), None)
+    return expense_num
--- start_bot.py
+++ start_bot.py
@@ -1,9 +1,12 @@
 import logging
 import datetime
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
-from settings import API_TOKEN  # Плохо, заменить на переменную окружения
-from family_funds import get_command_descriptions, check_outlay_string, make_outlay, add_outlay, format_outlays_report
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+from settings import API_TOKEN
+from family_funds import get_command_descriptions, check_expense_string
+from family_funds import add_category, add_categories, show_categories
+from family_funds import make_expense, add_expense, format_expenses_report, find_expense_num_by_id
+from family_funds import save_user_data, load_user_data
 
 
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
@@ -11,7 +14,7 @@ logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     filename='bot.log')
 
 
-def start_handler(update, context): # Тут будут начальные настройки. Или не будут? Продумать механизм, который не позволит одной командой обнулить свои данные
+def start_handler(update, context):
     update.message.reply_text('Привет! Я помогаю вести семейный бюджет! Вот список доступных команд:', quote=False)
     update.message.reply_text(get_command_descriptions(), quote=False)
 
@@ -21,19 +24,66 @@ def help_handler(update, context):
     update.message.reply_text(get_command_descriptions(), quote=False)
 
 
-def outlay_add_handler(update, context):
-    outlays = context.chat_data.get('outlays')
-    feedback = check_outlay_string(update.message.text)
+def make_categories_buttons(categories, new_expense):
+    button_list = []
+    for category in categories:
+        button = InlineKeyboardButton(category.title(), callback_data = new_expense['id'] + category)
+        button_list.append(button)
+    reply_markup = InlineKeyboardMarkup([button_list[i:i + 3] for i in range(0, len(button_list), 3)])
+    return reply_markup
+
+
+def expense_add_handler(update, context):
+    context.chat_data.update(load_user_data(update.message.chat.id))
+    expenses = context.chat_data.get('expenses')
+    feedback = check_expense_string(update.message.text)
     if feedback == 'ok':
-        new_outlay = make_outlay(update.message.text)
-        context.chat_data['outlays'], feedback = add_outlay(outlays, new_outlay)
+        new_expense = make_expense(update.message.text)
+        context.chat_data['expenses'], feedback = add_expense(expenses, new_expense)
+        reply_markup = make_categories_buttons(context.chat_data['categories'], new_expense)
+        save_user_data(context.chat_data, update.message.chat.id)
+        update.message.reply_text(feedback, quote=False, reply_markup=reply_markup)
+    else:    
+        update.message.reply_text(feedback, quote=False)
+
+
+def set_expense_category(update, context):
+    target_id = update.callback_query.data[:36]
+    target_category = update.callback_query.data[36:]
+    context.chat_data.update(load_user_data(update.callback_query.message.chat.id))
+    update.callback_query.answer('Отличный выбор!')
+    expense_num = find_expense_num_by_id(context.chat_data['expenses'], target_id)
+    context.chat_data['expenses'][expense_num]['category'] = target_category
+    save_user_data(context.chat_data, update.callback_query.message.chat.id)
+    update.callback_query.edit_message_text(text=f"Расход добавлен в категорию {target_category.title()}")
+
+
+def add_category_handler(update, context):
+    context.chat_data.update(load_user_data(update.message.chat.id))
+    categories = context.chat_data.get('categories')
+    context.chat_data['categories'], feedback = add_category(categories, update.message.text)
+    save_user_data(context.chat_data, update.message.chat.id)
     update.message.reply_text(feedback, quote=False)
 
 
-def show_outlays_handler(update, context):
-    outlays_report = format_outlays_report(context.chat_data['outlays'])
-    update.message.reply_text('Cписок ваших расходов:', quote=False)
-    update.message.reply_text(outlays_report, quote=False)
+def add_categories_handler(update, context):
+    context.chat_data.update(load_user_data(update.message.chat.id))
+    categories = context.chat_data.get('categories')
+    context.chat_data['categories'], feedback = add_categories(categories, update.message.text)
+    save_user_data(context.chat_data, update.message.chat.id)
+    update.message.reply_text(feedback, quote=False)
+
+
+def show_expenses_handler(update, context):
+    context.chat_data.update(load_user_data(update.message.chat.id))
+    expenses_report = format_expenses_report(context.chat_data['expenses'], update.message.chat.id)
+    update.message.reply_text(expenses_report, quote=False)
+
+
+def show_categories_handler(update, context):
+    context.chat_data.update(load_user_data(update.message.chat.id))
+    categories_report = show_categories(context.chat_data.get('categories'))
+    update.message.reply_text(categories_report, quote=False)
 
 
 def main():
@@ -41,8 +91,12 @@ def main():
     dp = ffbot.dispatcher
     dp.add_handler(CommandHandler('start', start_handler))
     dp.add_handler(CommandHandler('help', help_handler))    
-    dp.add_handler(CommandHandler('add', outlay_add_handler))
-    dp.add_handler(CommandHandler('outlays', show_outlays_handler))
+    dp.add_handler(CommandHandler('add', expense_add_handler))
+    dp.add_handler(CallbackQueryHandler(set_expense_category))
+    dp.add_handler(CommandHandler('add_cat', add_category_handler))
+    dp.add_handler(CommandHandler('add_cats', add_categories_handler))
+    dp.add_handler(CommandHandler('my_cats', show_categories_handler))  
+    dp.add_handler(CommandHandler('expenses', show_expenses_handler))
     logging.info(f'\n\n\n{datetime.datetime.now()}: Bot has started')
     ffbot.start_polling()
     ffbot.idle()
