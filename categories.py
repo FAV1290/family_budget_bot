b0f4d625f1d3585169f3a2a714c272867fe2da47
Alexey <fav1290@gmail.com>
2023-07-12 11:47:23 +0300

    Preparations for adding budgets and limits system

--- categories.py
+++ categories.py
@@ -1,36 +1,46 @@
-from database_handlers import update_categories, get_user_categories
+import uuid
+from database_handlers import get_user_categories, save_new_category
 
 
-def check_categories_input(user_id, user_input):
-    feedback = ''
-    categories_list = get_user_categories(user_id)
-    for category in map(str.strip, user_input.partition(' ')[2].lower().split(',')):
-        if category in categories_list:
-            feedback = feedback + f'• Категория {category.capitalize()} уже существует\n'
-        elif category == '' or len(category) > 50:
-            continue
-        else:
-            feedback = feedback +  f'• Категория {category.capitalize()} добавлена\n'
-            categories_list.append(category)
-    if feedback == '':
-        feedback = ''.join(
-            [
-                'Не удалось добавить ни одной категории.',
-                '\nВозможно, вы ввели некорректные данные?',
-            ]
-        )
-    return categories_list, feedback
+def get_user_categories_names(user_id):
+    return [category['name'] for category in get_user_categories(user_id)]
+
+
+def check_category_input(user_id, user_input):
+    category, feedback = user_input.lower().strip(), 'ok'
+    categories_list = get_user_categories_names(user_id)
+    if category in categories_list:
+        feedback = f'Категория {category.capitalize()} уже существует'
+    elif category == '' or len(category) > 50:
+        feedback = 'Не удалось добавить категорию. Возможно, вы ввели некорректные данные?'
+    return feedback
+
+
+def make_category_dict(user_id, user_input):
+    category = {
+        'user_id' : user_id,
+        'category_id' : uuid.uuid4(),
+        'name' : user_input,
+        'limit' : None,
+    }
+    return category
 
 
 def add_category(user_id, user_input):
-    categories_list, feedback = check_categories_input(user_id, user_input)
-    update_categories(user_id, categories_list)
+    feedback = check_category_input(user_id, user_input)
+    if feedback != 'ok':
+        return feedback
+    new_category = make_category_dict(user_id, user_input)
+    feedback = f'Категория {new_category["name"].capitalize()} успешно добавлена'
+    save_new_category(new_category)
     return feedback
 
 
 def make_categories_report(user_id):
     categories_list = get_user_categories(user_id)
     report = 'Вам доступны следующие категории расходов:'
-    for category in sorted(categories_list):
-        report += f'\n • {category.capitalize()}'
+    for category in sorted(categories_list, key = lambda category:category['name']):
+        report += f'\n • {category["name"].capitalize()}'
+        if category['limit'] is not None:
+            report += f' (Лимит: {category["limit"]} руб.)'
     return report
--- first_launch.py
+++ first_launch.py
@@ -1,14 +1,147 @@
 import datetime
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import (
-    CommandHandler, CallbackQueryHandler, ConversationHandler, MessageHandler, Filters
-)
-from categories import add_category
-from database_handlers import update_categories, update_utc_offset, get_user_config_status, update_config_status
-from constants import REGIONS, UTC_OFFSETS_DIVISION, COMMANDS, DEFAULT_CATEGORIES
+    CommandHandler, CallbackQueryHandler, ConversationHandler, MessageHandler, Filters)
+from categories import add_category 
+from database_handlers import update_utc_offset, get_user_settings, update_config_status
+from constants import REGIONS, UTC_OFFSETS_DIVISION, COMMANDS
 
 
 NOT_FIRST_LAUNCH, REGION, UTC_OFFSET, CATEGORIES = range(4)
+
+
+def make_regions_buttons():
+    button_list = [InlineKeyboardButton(
+        key.title().replace('И', 'и'), callback_data = value) for key, value in REGIONS]
+    reply_markup = InlineKeyboardMarkup(
+        [button_list[index:index + 2] for index in range(0, len(button_list), 2)])
+    return reply_markup
+
+
+def make_utc_buttons(region):
+    button_list = []
+    for offset in UTC_OFFSETS_DIVISION[region]:
+        if offset > 0:
+            title = '+' + str(offset)
+        else:
+            title = str(offset)
+        button = InlineKeyboardButton(title, callback_data = offset)
+        button_list.append(button)
+    reply_markup = InlineKeyboardMarkup(
+        [button_list[index:index + 3] for index in range(0, len(button_list), 3)])
+    return reply_markup
+
+
+def make_reinit_settings_buttons():
+    button_list = [InlineKeyboardButton(
+        key, callback_data = value) for key, value in [('Да', 'yes'), ('Нет', 'no')]]
+    reply_markup = InlineKeyboardMarkup([button_list])
+    return reply_markup
+
+
+def start_greetings_stage(update, context):
+    config_status = get_user_settings(update.message.chat.id)['is_app_configured']
+    if config_status:
+        bot_answer = ' '.join([
+            'Привет! Если не ошибаюсь, мы с вами уже зафиксировали первоначальные настройки.',
+            'Уверены, что хотите повторить процесс?',
+        ])
+        reinit_buttons = make_reinit_settings_buttons()
+        update.message.reply_text(bot_answer, quote=False, reply_markup = reinit_buttons)
+        return NOT_FIRST_LAUNCH     
+    else:
+        bot_answer = [
+            'Привет! Я помогаю вести семейный бюджет!',
+            'Начнем знакомство с настроек',
+        ]
+        update.message.reply_text(' '.join(bot_answer), quote=False)
+        region_question = 'Сперва выберите регион, в котором находитесь:'
+        regions_buttons = make_regions_buttons()
+        update.message.reply_text(region_question, quote=False, reply_markup = regions_buttons)
+        return REGION
+
+
+def start_reinit_stage(update, context):
+    user_answer = update.callback_query.data
+    if user_answer == 'yes':
+        update_config_status(update.callback_query.message.chat.id, False)
+        region_question = 'Да будет так. Сперва выберите регион, в котором находитесь:'
+        regions_buttons = make_regions_buttons()
+        update.callback_query.edit_message_text(region_question, reply_markup = regions_buttons())
+        return REGION
+    else:
+        update.callback_query.edit_message_text('Тогда отбой. До скорых встреч!')
+        return ConversationHandler.END
+
+
+def start_region_stage(update, context):
+    region = update.callback_query.data
+    update.callback_query.answer('Всё с вами ясно!')
+    utc_question = ''.join(
+        [
+            'Теперь укажите отклонение времени от UTC в вашем регионе ',
+            '(Например, московское время - UTC+3):'
+        ])
+    update.callback_query.edit_message_text(utc_question, reply_markup = make_utc_buttons(region))
+    return UTC_OFFSET
+
+
+def start_utc_offset_stage(update, context):
+    offset = int(update.callback_query.data)
+    update.callback_query.answer('Настройка сохранена!')
+    estimated_date = (
+        datetime.datetime.utcnow() + datetime.timedelta(hours=offset)).strftime('%d.%m.%y, %H:%M')
+    bot_answer = [
+        f'Принято! Согласно этим данным, сейчас у вас {estimated_date}.',
+        '\nТеперь давайте добавим какую-нибудь категорию расходов.'
+        '\nВведите ее название или /cancel,',
+        'чтобы отложить это действие',
+        '(Позднее вы сможете добавлять категории с помощью команды /new_category).',
+    ]
+    update.callback_query.edit_message_text(' '.join(bot_answer))
+    update_utc_offset(update.callback_query.message.chat.id, offset)
+    return CATEGORIES
+
+
+def make_bot_farewell_speach(categories_skipped = False):
+    bot_answer = [
+        'Вас понял, пока что никаких новых категорий.',
+        'А начальная настройка, тем временем, завершена.',
+        'Приятного вам использования и вкратце о командах:',
+        '\n• /categories - посмотреть категории расходов',
+        '\n• /new_category - добавить категорию расходов',
+        '\n• /add - добавить расход',        
+        '\n• /help - посмотреть все доступные команды', 
+    ]
+    if not categories_skipped:
+        bot_answer.pop(0)
+    return ' '.join(bot_answer)
+
+
+def skip_categories_stage(update, context):
+    bot_answer = make_bot_farewell_speach(categories_skipped=True)
+    update.message.reply_text(bot_answer, quote=False)
+    update_config_status(update.message.chat.id, True)
+    return ConversationHandler.END
+
+
+def start_categories_stage(update, context):
+    categories_report = add_category(update.message.chat.id, update.message.text)
+    update.message.reply_text(categories_report, quote=False)
+    bot_answer = make_bot_farewell_speach()
+    update.message.reply_text(bot_answer, quote=False)
+    update_config_status(update.message.chat.id, True)
+    return ConversationHandler.END
+
+
+def start_cancel_stage(update, context):
+    bot_message = [
+        'Похоже, вам приглянулась другая команда. Сейчас я выйду из режима настроек,',
+        'чтобы ее можно было использовать.',
+        'Если же захотите вернуться к настройкам, введите "/start"',
+    ]
+    update.message.reply_text(' '.join(bot_message), quote=False)
+    return ConversationHandler.END
 
 
 def start_handler():
@@ -27,123 +160,3 @@ def start_handler():
         per_user=False,
     )
     return start_handler
-
-
-def make_regions_buttons():
-    button_list = [InlineKeyboardButton(key.title().replace('И', 'и'), callback_data = value) for key, value in REGIONS]
-    reply_markup = InlineKeyboardMarkup(
-        [button_list[index:index + 2] for index in range(0, len(button_list), 2)])
-    return reply_markup
-
-
-def make_utc_offset_buttons(region):
-    button_list = []
-    for offset in UTC_OFFSETS_DIVISION[region]:
-        if offset > 0:
-            title = '+' + str(offset)
-        else:
-            title = str(offset)
-        button = InlineKeyboardButton(title, callback_data = offset)
-        button_list.append(button)
-    reply_markup = InlineKeyboardMarkup([button_list[index:index + 3] for index in range(0, len(button_list), 3)])
-    return reply_markup
-
-
-def make_reinit_settings_buttons():
-    button_list = [InlineKeyboardButton(key, callback_data = value) for key, value in [('Да', 'yes'), ('Нет', 'no')]]
-    reply_markup = InlineKeyboardMarkup([button_list])
-    return reply_markup
-
-
-def start_greetings_stage(update, context):
-    config_status = get_user_config_status(update.message.chat.id)
-    if config_status:
-        bot_answer = [
-            'Привет! Если не ошибаюсь, мы с вами уже зафиксировали первоначальные настройки.',
-            'Уверены, что хотите повторить процесс? (Это может привести к потере каких-либо данных)',
-        ]
-        update.message.reply_text(' '.join(bot_answer), quote=False, reply_markup = make_reinit_settings_buttons())
-        return NOT_FIRST_LAUNCH     
-    else:
-        update.message.reply_text('Привет! Я помогаю вести семейный бюджет! Предлагаю начать знакомство с настроек', quote=False)
-        region_question = 'Сперва выберите регион, в котором находитесь:'
-        update.message.reply_text(region_question, quote=False, reply_markup = make_regions_buttons())
-        return REGION
-
-
-def start_reinit_stage(update, context):
-    user_answer = update.callback_query.data
-    if user_answer == 'yes':
-        update_config_status(update.callback_query.message.chat.id, False)
-        region_question = 'Да будет так. Сперва выберите регион, в котором находитесь:'
-        update.callback_query.edit_message_text(region_question, reply_markup = make_regions_buttons())   
-        return REGION
-    else:
-        update.callback_query.edit_message_text('Тогда отбой. До скорых встреч!')
-        return ConversationHandler.END
-
-
-def start_region_stage(update, context):
-    region = update.callback_query.data
-    update.callback_query.answer('Всё с вами ясно!')
-    utc_question = 'Теперь укажите отклонение времени от UTC в вашем регионе (Например, московское время - UTC+3):'
-    update.callback_query.edit_message_text(utc_question, reply_markup = make_utc_offset_buttons(region))
-    return UTC_OFFSET
-
-
-def start_utc_offset_stage(update, context):
-    offset = int(update.callback_query.data)
-    update.callback_query.answer('Настройка сохранена!')
-    estimated_date = (datetime.datetime.utcnow() + datetime.timedelta(hours=offset)).strftime('%d.%m.%y, %H:%M')
-    bot_answer = [
-        f'Принято! Согласно этим данным, сейчас у вас {estimated_date}.',
-        '\nВнимание! На этом шаге список категорий (если вы их уже добавили) может быть обнулен.'
-        '\nВведите через запятую категории своих расходов',
-        'или /cancel, чтобы добавить их потом (или сохранить текущие категории)',
-        '(Позднее вы сможете добавить категории с помощью команды /add_category).',
-
-    ]
-    update.callback_query.edit_message_text(' '.join(bot_answer))
-    update_utc_offset(update.callback_query.message.chat.id, offset)
-    return CATEGORIES
-
-
-def make_bot_farewell_speach(categories_skipped = False):
-    bot_answer = [
-        'Вас понял, пока что никаких новых категорий.',
-        'А начальная настройка, тем временем, завершена.',
-        'Приятного вам использования и вкратце о командах:',
-        '\n• /categories - посмотреть категории расходов',
-        '\n• /add [сумма] [комментарий - необязательно] - добавить расход',        
-        '\n• /help - посмотреть все доступные команды', 
-    ]
-    if not categories_skipped:
-        bot_answer.pop(0)
-    return ' '.join(bot_answer)
-
-
-def skip_categories_stage(update, context):
-    bot_answer = make_bot_farewell_speach(categories_skipped=True)
-    update.message.reply_text(bot_answer, quote=False)
-    update_config_status(update.message.chat.id, True)
-    return ConversationHandler.END
-
-
-def start_categories_stage(update, context):
-    update_categories(update.message.chat.id, DEFAULT_CATEGORIES)
-    categories_report = add_category(update.message.chat.id, '/add_category ' + update.message.text)
-    update.message.reply_text(categories_report, quote=False)
-    bot_answer = make_bot_farewell_speach()
-    update.message.reply_text(bot_answer, quote=False)
-    update_config_status(update.message.chat.id, True)
-    return ConversationHandler.END
-
-
-def start_cancel_stage(update, context):
-    bot_message = [
-        'Похоже, вам приглянулась другая команда. Сейчас я выйду из режима настроек,',
-        'чтобы ее можно было использовать.',
-        'Если же захотите вернуться к настройкам, введите "/start"',
-    ]
-    update.message.reply_text(' '.join(bot_message), quote=False)
-    return ConversationHandler.END
