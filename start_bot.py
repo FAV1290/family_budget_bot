c5dd144c4551071bdc529da2cf35648c91948b9d
Alexey <fav1290@gmail.com>
2023-07-15 10:50:46 +0300

    demo build

--- start_bot.py
+++ start_bot.py
@@ -1,7 +1,7 @@
 import logging
 import datetime
 from telegram.ext import Updater, CommandHandler
-from constants import API_TOKEN, COMMANDS
+from constants import API_TOKEN, COMMANDS, QUICK_START, START_SPEECH
 from apps.categories import make_categories_report
 from apps.expenses import make_expenses_report
 from bot.first_launch_dialog import start_handler
@@ -13,6 +13,10 @@ from bot.utc_offset_dialog import add_utc_offset_handler
 logging.basicConfig(format='%(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO,
                     filename='bot.log')
+
+
+def hello_handler(update, context):
+    update.message.reply_text(START_SPEECH, quote=False)
 
 
 def get_command_descriptions():
@@ -40,7 +44,10 @@ def show_categories_handler(update, context):
 def main():
     ffbot = Updater(API_TOKEN, use_context=True)
     dp = ffbot.dispatcher
-    dp.add_handler(start_handler()) #s
+    if QUICK_START:
+        dp.add_handler(CommandHandler('start', hello_handler)) #s 
+    else:
+        dp.add_handler(start_handler()) #s
     dp.add_handler(add_expense_handler()) #a
     dp.add_handler(add_category_handler()) #n
     dp.add_handler(add_utc_offset_handler()) #u
